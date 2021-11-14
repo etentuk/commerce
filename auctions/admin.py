ed85cb9d2e7ed249e402efb0940cd6dd2b0d3948
Etentuk <etentuke@gmail.com>
2021-11-14 14:27:12 +0100

    created auction and category models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
+from typing import List
 from django.contrib import admin
+from .models import Categories, Listing, User
 
 # Register your models here.
+admin.site.register(Listing)
+admin.site.register(Categories)
+admin.site.register(User)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE, SET
 
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.IntegerField()
+    image_url = models.URLField(blank=True)
+    category = models.ForeignKey(
+        'Categories', blank=True, on_delete=SET(""), related_name='listings')
+    owner = models.ForeignKey(
+        User, on_delete=CASCADE, related_name="listings"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=64)
